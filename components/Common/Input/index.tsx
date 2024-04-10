f7accd73ba87826d9787286aa8ee84fe9aa02678
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-04-10 19:13:27 +0200

    feat(input): introduce

---
+++ components/Common/Input/index.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+import styles from './index.module.css';
+import type { FC, InputHTMLAttributes } from 'react';
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  inline?: boolean;
+};
+
+const Input: FC<InputProps> = ({ label, inline, type, ...inputProps }) => (
+  <div
+    className={classNames(styles.inputWrapper, {
+      [styles.inline]: inline,
+      [styles.checkbox]: type === 'checkbox',
+    })}
+  >
+    {label && <label className={styles.label}>{label}</label>}
+    <input className={styles.input} type={type} {...inputProps} />
+  </div>
+);
+
+
+export default Input;
